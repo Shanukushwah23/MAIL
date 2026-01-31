13666f288c404c9c5fd637b4585fcc4136f0586b
Shanu kushwah <151635109+Shanukushwah23@users.noreply.github.com>
2026-01-31 17:34:33 +0530

    Update MyController.java

--- Mail-Sending/src/main/java/com/kushwah/shanu/controller/MyController.java
+++ Mail-Sending/src/main/java/com/kushwah/shanu/controller/MyController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 import com.kushwah.shanu.entity.BookingRequest;
 import com.kushwah.shanu.service.EmailService;
@@ -33,6 +35,7 @@ public class MyController {
         return ResponseEntity.ok("Booking sent");
     }
 }
+
 
 
 
